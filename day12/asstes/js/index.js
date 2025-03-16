147bda5db2335bf0a9822665892e135fb20ba809
dimuthx353 <dimuthx353@gmail.com>
2025-03-16 17:10:18 +0530

    add new activities

---
+++ day12/asstes/js/index.js
@@ -0,0 +1,61 @@
+// let person = "John Doe", carName = "Volvo", price = 200;
+
+// let person ="John Doe";
+// let carName = "Volvo"
+// let price = 200;
+
+// let person = "John Doe", carName = "Volvo", price = 200,country="sl";
+
+// let num01 = 10;
+// let num02 = 20;
+// let total = num01+num02;
+
+// console.log(total);
+
+// console.log("num01");
+// console.log(num01);
+// console.log("num02");
+// console.log(num02);
+
+// console.log('total');
+// console.log(num01+num02);
+// console.log(num02-num01);
+
+
+// let result = true;
+// let result2 = false;
+
+// console.log(result);
+// console.log(result2);
+
+// console.log(result , result2, num01 , num02);
+
+// console.log(`'hhhhhe'`);
+// let text = "We are the so-called \"Vikings\" from the north.";
+// console.log(text); 
+
+// `
+
+// let firstName = "dimuth";
+// let lastName ="adithya";
+// let num =10;
+// let bValue = true;
+
+// console.log(typeof firstName ,typeof lastName );
+// console.log(typeof num);
+// console.log(typeof bValue);
+
+
+
+
+
+// let space = " ";
+// let fullName = firstName+lastName;
+// let fullName = firstName+space+lastName;
+// let fullName = firstName + " " + lastName;
+
+
+
+// console.log(fullName);
+// console.log(firstName ,lastName);
+
